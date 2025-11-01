a9cfe7887c1583dca1c29e3705d95710f80b08d2
Eduardo CABALLERO <e.caballerol@uniandes.edu.co>
2025-11-01 15:06:48 +0100

    Punto 1 completado: Módulo, clase, servicio y componente de vehículos funcionando

--- src/app/vehiculo/vehiculo-list/vehiculo-list.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from '../vehiculo';
+import { VehiculoService } from '../vehiculo.service';
 
 @Component({
   selector: 'app-vehiculo-list',
-  imports: [],
   templateUrl: './vehiculo-list.html',
-  styleUrl: './vehiculo-list.css',
+  styleUrls: ['./vehiculo-list.css']
 })
-export class VehiculoList {
+export class VehiculoListComponent implements OnInit {
+  vehiculos: Array<Vehiculo> = [];
 
+  constructor(private vehiculoService: VehiculoService) { }
+
+  ngOnInit() {
+    this.getVehiculos();
+  }
+
+  getVehiculos(): void {
+    this.vehiculoService.getVehiculos().subscribe(vehiculos => {
+      this.vehiculos = vehiculos;
+    });
+  }
 }
--- src/app/vehiculo/vehiculo-module.ts
+++ src/app/vehiculo/vehiculo-module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { VehiculoListComponent } from './vehiculo-list/vehiculo-list';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    VehiculoListComponent
+  ],
+  exports: [
+    VehiculoListComponent
   ]
 })
 export class VehiculoModule { }
