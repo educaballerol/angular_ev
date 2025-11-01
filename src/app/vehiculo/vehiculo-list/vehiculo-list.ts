809e2d8eb4c57e57d0e45a36a73c2d7dc90e14c5
Eduardo CABALLERO <e.caballerol@uniandes.edu.co>
2025-11-01 14:30:53 +0100

    creacion de componente, modulo, servicio y lista de vehiculos

---
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehiculo-list',
+  imports: [],
+  templateUrl: './vehiculo-list.html',
+  styleUrl: './vehiculo-list.css',
+})
+export class VehiculoList {
+
+}
